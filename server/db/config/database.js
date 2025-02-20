488cd615c8094e7000559dc0913199bc8a24a7a5
Justice Foster <omario2005x@gmail.com>
2025-02-19 22:45:59 -0500

    got rid of config.json, added .sequleizerc

--- server/db/config/database.js
+++ server/db/config/database.js
@@ -14,9 +14,10 @@ const sequelize = new Sequelize(
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false, // ✅ Required for Render
+        rejectUnauthorized: false,
       },
     },
+    logging: false,
   }
 );
 
