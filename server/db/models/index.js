9209a3693369c86ffdde9d12b5a4b9119aefe19a
Justice Foster <omario2005x@gmail.com>
2025-02-19 22:53:32 -0500

    Moved database.js to correct path

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,4 @@
-import sequelize from "../config/database.js";
+import sequelize from "../../config/database.js";
 import dotenv from 'dotenv';
 import User from './User.js';
 import Coffee from './Coffee.js';
